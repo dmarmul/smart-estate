794219ede55a090a519d3e509750f5ee4f000984
Dmytro <156954070+dmarmul@users.noreply.github.com>
2024-12-06 23:05:19 +0200

    Delete src directory

--- src/components/Loader/Loader.tsx
+++
@@ -1,7 +0,0 @@
-import s from './Loader.module.css';
-
-export const Loader = () => (
-  <div className={s.Loader} data-cy="Loader">
-    <div className={s.Loader__content} />
-  </div>
-);
--- src/pages/NeighborhoodsPage.tsx
+++
@@ -1,3 +0,0 @@
-export const NeighborhoodPage: React.FC = () => {
-  return <></>;
-};
